b13766086bc705eb0fbc58504a6a7238225c3595
YA <9039417+yzkn@users.noreply.github.com>
2024-11-02 15:39:56 +0900

    succeeded

--- BlazorDataverse/Program.cs
+++ BlazorDataverse/Program.cs
@@ -1,11 +1,65 @@
-using BlazorDataverse;
-using Microsoft.AspNetCore.Components.Web;
+using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using BlazorDataverse.Models;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
+namespace BlazorDataverse
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-await builder.Build().RunAsync();
+            // Get configuration data about the Global Discovery API set in wwwroot/appsettings.json
+            var GDSWebApiConfig = builder.Configuration.GetSection("GDSWebAPI");
+            var GDSBaseUrl = GDSWebApiConfig.GetSection("BaseUrl").Value;
+            var GDSVersion = GDSWebApiConfig.GetSection("Version").Value;
+
+            // Get configuration data about the Web API set in wwwroot/appsettings.json
+            var CDSWebApiConfig = builder.Configuration.GetSection("CDSWebAPI");
+            var baseUrl = CDSWebApiConfig.GetSection("BaseUrl").Value;
+            var version = CDSWebApiConfig.GetSection("Version").Value;
+            var timeoutSeconds = int.Parse(CDSWebApiConfig.GetSection("TimeoutSeconds").Value);
+
+            // Create an named definition of an HttpClient that can be created in a component page
+            builder.Services.AddHttpClient("GDSClient", client =>
+            {
+                client.Timeout = TimeSpan.FromSeconds(timeoutSeconds);
+                client.BaseAddress = new Uri($"{GDSBaseUrl}");
+            });
+
+            // Create an named definition of an HttpClient that can be created in a component page
+            builder.Services.AddHttpClient("CDSClient", client =>
+            {
+                // See https://learn.microsoft.com/powerapps/developer/common-data-service/webapi/compose-http-requests-handle-errors                
+                client.Timeout = TimeSpan.FromSeconds(timeoutSeconds);
+                client.DefaultRequestHeaders.Add("Accept", "application/json");
+                client.DefaultRequestHeaders.Add("OData-Version", "4.0");
+                client.DefaultRequestHeaders.Add("OData-MaxVersion", "4.0");
+                client.BaseAddress = new Uri($"{baseUrl}/api/data/{version}");
+            });
+
+            builder.Services.AddMsalAuthentication(options =>
+            {
+                builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
+
+                // Add access to Global Discovery Service to the scope of the access token when the user signs in                
+                options.ProviderOptions.DefaultAccessTokenScopes.Add($"{GDSBaseUrl}/user_impersonation");
+            });
+
+            builder.Services.AddSingleton(typeof(AppState), new AppState());
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
